186af0a00818ae250cb7405c2bbb27ca0ce3a904
Dushyant Garg <147367191+970dishu@users.noreply.github.com>
2024-09-19 11:39:18 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all js code as newer version of code
+
+// alert("hello") // we are using node.js not browser
+
+console.log(3+3) 
+//code readablility should be high
+console.log("hitesh")
