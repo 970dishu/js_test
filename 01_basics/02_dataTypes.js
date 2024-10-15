ce126b9e45c306941e64807af6157862ab84abcc
Dushyant Garg <147367191+970dishu@users.noreply.github.com>
2024-10-15 10:39:45 +0000

    datatypes done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,3 +5,22 @@
 console.log(3+3) 
 //code readablility should be high
 console.log("hitesh")
+
+let name = "Hitesh";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
